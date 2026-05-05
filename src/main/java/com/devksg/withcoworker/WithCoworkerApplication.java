db3cf38975e53eba3e429a77864862173eeecf94
lookatthepinksky <bitwinsns@naver.com>
2026-05-05 20:37:14 +0900

    first commit

---
+++ src/main/java/com/devksg/withcoworker/WithCoworkerApplication.java
@@ -0,0 +1,13 @@
+package com.devksg.withcoworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithCoworkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithCoworkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devksg/withcoworker/WithCoworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devksg.withcoworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithCoworkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
